029bb01053c49859af2b6a7217745ac3c2ef7d0a
Jared-P-111 <jaredpisell@gmail.com>
2023-02-06 12:37:14 -0800

    Added first java program

---
+++ Java Fundamentals/FirstJavaProgram/firstJavaProgram.java
@@ -0,0 +1,8 @@
+public class firstJavaProgram {
+
+  public static void main(String[] args) {
+    System.out.println("Jared Pisell");
+    System.out.println("41");
+    System.out.println("Downey");
+  }
+}
