86269fe4cde004aa023e5f9e121326c643390c74
Jared-P-111 <jaredpisell@gmail.com>
2023-02-13 11:27:06 -0800

    Added Hello_Human to repo

---
+++ Spring/hello_human/src/main/java/com/hello_human/hello_human/MainController.java
@@ -0,0 +1,16 @@
+package com.hello_human.hello_human;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+  @RequestMapping("")
+  public String index(
+    @RequestParam(value = "name", required = false) String searchQuery
+  ) {
+    return "Hello " + searchQuery;
+  }
+}
